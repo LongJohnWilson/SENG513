12ded43d74a53dc2b6efcded02a4167c19889edf
LongJohnWilson <johnwilson12321@gmail.com>
2019-02-14 17:15:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const calculator = document.querySelector(‘.calculator’)
-const keys = calculator.querySelector(‘.calculator__keys’)
-keys.addEventListener(‘click’, e => {
- if (e.target.matches(‘button’)) {
-   console.log("aahhhhhh")
- }
-})
+console.log("hello world")
